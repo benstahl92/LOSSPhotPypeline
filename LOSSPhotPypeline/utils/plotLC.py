74c37b9ea0d5ef49b9a48750babd8ae937e1b0eb
Benjamin Stahl <benstahl92@gmail.com>
2018-09-20 14:00:38 -0700

    added interactive point cutting capability

--- LOSSPhotPypeline/utils/plotLC.py
+++ LOSSPhotPypeline/utils/plotLC.py
@@ -1,11 +1,41 @@
+# standard package imports
+import copy
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def onpick(event, df, drop_dict, filt, cut, fig, offset):
+    '''
+    Handles click events by plot_lc.
+
+    Parameters
+    ----------
+    event : matplotlib click event
+    df : pandas.DataFrame
+        internal light curve format with non-null magnitude values in 'filt'
+    drop_dict : dict
+        dict of format filt: [<indices to be cut from df>]
+    cut : matplotlib plot instance
+        plot to add cut points to
+    fig : matplotlib figure
+        figure instance
+    offset : int or float
+        (filter dependent) magnitude offset
+    '''
+
+    # get index of event and use it to add appropriate id to drop_dict
+    ind = event.ind[0]
+    drop_dict[filt].append(df.index[ind])
+
+    # plot the location
+    x = df['t_rel'].loc[drop_dict[filt]]
+    y = df[filt].loc[drop_dict[filt]] + offset
+    cut.set_data(x, y)
+    fig.canvas.draw()
+
 class plotLC:
-    '''
-    Light curve plotting for LOSSPhotPypeline outputs
-    '''
+    '''Light curve plotting for LOSSPhotPypeline outputs'''
 
     def __init__(self, lc = None, lc_raw = None, lc_file = None, tref = 'min', filters = ('B','V','R','I','CLEAR'), 
                  offset_scale = 1, style = 'white', context = 'notebook', name = None, photmethod = None):
@@ -17,7 +47,7 @@ class plotLC:
         lc : pandas.DataFrame, optional, default: None
             internal representation of light curve
             must have columns: MJD, B, EB, V, EV, R, ER, I, EI, CLEAR, ECLEAR
-        lc : pandas.DataFrame, optional, default: None
+        lc_raw : pandas.DataFrame, optional, default: None
             dataframe in "raw" light curve format
         lc_file : str, optional, default: None
             name of file containing light curve to plot
@@ -25,10 +55,16 @@ class plotLC:
             time to reference all others to
         filters : iterable, optional, default: ('B','V','R','I','CLEAR')
             photometric passbands to plot
+        offset_scale : int or float, optional, default: 1
+            multiplicative factor to set magnitude offset scale
         style : str, optional, default: 'white'
             seaborn plot style to use
         context : str, optional, default: 'notebook'
             seaborn context to use
+        name : str, optional, default: None
+            name of object
+        photmethod : str, optional, default: None
+            photometry method used in generating light curve
         '''
 
         self.lc = lc
@@ -42,6 +78,7 @@ class plotLC:
         self.photmethod = photmethod
 
         self.tset = False
+        self.lc_cut = None
 
         filters = [filt.upper() for filt in filters]
         if set(filters).issubset(('B','V','R','I','CLEAR')) is False:
@@ -133,18 +170,41 @@ class plotLC:
             print('{} is an unrecognized light curve format, exiting')
             return
 
-    def plot_lc(self, style = None, context = None, return_fig = False):
+    def _drop_lc_points(self, drop_dict):
+        '''copy lc and then set all elements specified by dropdict to NaN'''
+        self.lc_cut = copy.deepcopy(self.lc)
+        for filt in drop_dict.keys():
+            self.lc_cut.loc[drop_dict[filt], filt] = np.nan
+
+    def _setup_plot(self):
+        '''does some of the bookkeeping needed to set up light curve plots'''
+        fig, ax = plt.subplots(1,1)
+        ax.invert_yaxis()
+        ax.set_xlabel('Time (MJD - {:.1f})'.format(self.tref))
+        ax.set_ylabel('Mag')
+        if self.name is not None:
+            title_msg = self.name
+            if self.photmethod is not None:
+                title_msg += ' ({})'.format(self.photmethod)
+            ax.set_title(title_msg)
+        return fig, ax
+
+    def plot_lc(self, lc = None, style = None, context = None, return_fig = False, icut = False):
         '''
         Plots light curve.
 
         Parameters
         ----------
+        lc : light curve in internal format, optional, default : None
+            light curve to plot
         style : str, optional, default: 'white'
             seaborn plot style to use
         context : str, optional, default: 'notebook'
             seaborn context to use
         return_fig : bool, optional, default : False
             return figure and axes if True
+        icut : bool, optional, default: False
+            run interactive point cutting
         '''
 
         # set plot attributes
@@ -155,28 +215,56 @@ class plotLC:
         sns.set_style(style)
         sns.set_context(context)
 
+        if lc is None:
+            lc = self.lc
+
         if self.tset is False:
             self._set_t()
 
-        # make plot
-        fig, ax = plt.subplots(1,1)
+        if icut is False:
+            fig, ax = self._setup_plot()
+        else:
+            drop_dict = {filt: [] for filt in self.filters}
+
+        # iterate through filters to generate light curves
         for filt in self.filters:
-            tmp = self.lc[self.lc[filt].notnull()]
+
+            if icut is True:
+                fig, ax = self._setup_plot()
+
+            # get non-null points in current passband
+            tmp = lc[self.lc[filt].notnull()]
+
+            # bookkeeping for current passband
             offset = self._offset(filt)
             sgn = '+'
             if offset < 0:
                 sgn = '-'
-            ax.errorbar(tmp['t_rel'], tmp[filt] + self._offset(filt), yerr = tmp['E' + filt], fmt = '.',
-                        c = self._color(filt), label = '{} {} {}'.format(filt, sgn, offset))
-        ax.legend()
-        ax.invert_yaxis()
-        ax.set_xlabel('Time (MJD - {:.1f})'.format(self.tref))
-        ax.set_ylabel('Mag')
-        if self.name is not None:
-            title_msg = self.name
-            if self.photmethod is not None:
-                title_msg += ' ({})'.format(self.photmethod)
-        if return_fig:
-            return fig, ax
+
+            # plot light curve
+            errobj = ax.errorbar(tmp['t_rel'], tmp[filt] + self._offset(filt), yerr = tmp['E' + filt], fmt = '.',
+                                 c = self._color(filt), label = '{} {} {}'.format(filt, sgn, abs(offset)), picker = 3)
+
+            # handle selection of bad points
+            if icut is True:
+                cut, = ax.plot([], [], 'rX', markersize = 12, label = 'points to cut')
+                ax.legend()
+                plt.ion()
+                cid = fig.canvas.mpl_connect('pick_event', lambda event: onpick(event, tmp, drop_dict, filt, cut, fig, offset))
+                fig.show()
+                input('hit [return] when done selection bad points in {} band > '.format(filt))
+                fig.canvas.mpl_disconnect(cid)
+                plt.ioff()
+                plt.close()
+                continue
+
+        if icut is True:
+            print(drop_dict)
+            self._drop_lc_points(drop_dict)
+            self.plot_lc(lc = self.lc_cut)
         else:
-            plt.savefig('{}.ps'.format(self.lc_file.split('.dat')[0]), bbox_inches = 'tight')
+            ax.legend()
+            if return_fig:
+                return fig, ax
+            else:
+                plt.savefig('{}.ps'.format(self.lc_file.split('.dat')[0]), bbox_inches = 'tight')
