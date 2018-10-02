2dfa0262c0584fc2051d33c60d9880b7affbe270
Benjamin Stahl <benstahl92@gmail.com>
2018-10-02 15:28:18 -0700

    removed unneeded kwd arg

--- LOSSPhotPypeline/image/Phot.py
+++ LOSSPhotPypeline/image/Phot.py
@@ -14,7 +14,7 @@ class Phot(FitsInfo):
 
     def __init__(self, name, radecfile = None, radec = None, quiet_idl = True, idl = None):
 
-        FitsInfo.__init__(self, name, autoload = True)
+        FitsInfo.__init__(self, name)
 
         self.radecfile = radecfile
         self.radec = radec
